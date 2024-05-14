4e3a414e6d88d653051dd938b6b4b0abac35fa07
Xenerain <Xenerain@gmail.com>
2024-05-14 15:09:26 +0700

    Initial commit

---
+++ src/main/java/org/xenerain/spawnerdrop/SpawnerDrop.java
@@ -0,0 +1,17 @@
+package org.xenerain.spawnerdrop;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SpawnerDrop extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
